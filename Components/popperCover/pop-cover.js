163f97cfcb2eecbbbaa41c847542ccb667a57fe7
phanhoanganh <hoanganh17b@gmail.com>
2019-08-15 23:42:08 +0700

    after trying

--- Components/popperCover/pop-cover.js
+++ Components/popperCover/pop-cover.js
@@ -4,24 +4,24 @@ const _html = function(title){
   
     <link rel="stylesheet" href="../../Components/popperCover/popperCover.css">
     
-        <div class = "style="padding: 0%; display: block">
-        <div id="draggable-scroll" class=" card ">
-            <h5 class="card-header success-color white-text">
-                <span comp-role ='title'>${title}</span>
-              <button type="button" class="close" aria-label="Close" comp-role = 'close'>
-                  <span aria-hidden="true">&times;</span>
-              </button></h5>
-            <div class="card-body">
-                <p class="card-text" id = "Context" >Providing Attribute Information</p>
-                <!-- Should be adding automatically -->
-                <slot name = 'docker'></slot>
-                <slot name = 'handler' class="card-text">
+        <div  style="padding: 0%; display: block">
+          <div id="draggable-scroll" class=" card ">
+              <h5 class="card-header success-color white-text">
+                  <span comp-role ='title'>${title}</span>
+                <button type="button" class="close" aria-label="Close" comp-role = 'close'>
+                    <span aria-hidden="true">&times;</span>
+                </button></h5>
+              <div class="card-body">
+                  <p class="card-text" id = "Context" >Providing Attribute Information</p>
+                  <!-- Should be adding automatically -->
+                  <slot name = 'docker'></slot>
+                  <slot name = 'handler' class="card-text">
+                    
+                  </slot>
                   
-                </slot>
-                
-            </div>
-        </div>
-  
+              </div>
+          </div>
+          <div class="popper__arrow"></div>
         </div>
         
     
