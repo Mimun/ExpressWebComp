6b1ace732b0ae63c0e40be61f05b0ce234829e6f
PhanHoangAnh <hoangnanh17b@gmail.com>
2019-08-19 15:37:10 +0700

    first steps of success

--- Components/popperCover/pop-cover.js
+++ Components/popperCover/pop-cover.js
@@ -4,25 +4,24 @@ const _html = function(title){
   
     <link rel="stylesheet" href="../../Components/popperCover/popperCover.css">
     
-        <div class = "popper"  style="padding: 0%; display: block">
-          <div id="draggable-scroll" class=" card ">
-              <h5 class="card-header success-color white-text">
-                  <span comp-role ='title'>${title}</span>
-                <button type="button" class="close" aria-label="Close" comp-role = 'close'>
-                    <span aria-hidden="true">&times;</span>
-                </button></h5>
-              <div class="card-body">
-                  <p class="card-text" id = "Context" >Providing Attribute Information</p>
-                  <!-- Should be adding automatically -->
-                  <slot name = 'docker'></slot>
-                  <slot name = 'handler' class="card-text">
-                    
-                  </slot>
-                  
-              </div>
-          </div>
-          <div class="popper__arrow"></div>
+    <div class = "popper"  style="padding: 0%; display: block">
+    <div id="draggable-scroll" class=" card ">
+        <h5 class="card-header success-color white-text">
+            <span comp-role ='title'>${title}</span>
+          <button type="button" class="close" aria-label="Close" comp-role = 'close'>
+              <span aria-hidden="true">&times;</span>
+          </button></h5>
+        <div class="card-body">
+            <p class="card-text" id = "Context" >Providing Attribute Information</p>
+            <!-- Should be adding automatically -->
+            <slot name = 'docker'></slot>
+            <slot name = 'handler' class="card-text">
+              
+            </slot>                
         </div>
+    </div>
+    <div x-arrow class="popper__arrow"></div>
+  </div>
         
     
   </template>
--- Components/popperCover/popperCover.js
+++ Components/popperCover/popperCover.js
@@ -1,23 +1,23 @@
 //var currentDocument = document.currentScript.ownerDocument;
 import _html from "./pop-cover.js";
+import  Popper from "../../node_modules/popper.js/dist/esm/popper.js";
+
+
 
 class popperCover extends HTMLElement {
     constructor() {
-        super();
-        this.addEventListener("click", (event) => {
-            // Do something here
-        }, false);
-        this.render();                        
+        super();        
+        
 
     }
     connectedCallback() {
-        
+        this.render();                        
     }
 
     static get observedAttributes() {
         return ['title', 'wcname'];
     }
-
+    // 
     attributeChangedCallback(name, oldVal, newVal) {
         console.log('attributeChangeCalback:', name, oldVal, newVal);
         if (name == 'title' && this.shadowRoot) {
@@ -39,13 +39,57 @@ class popperCover extends HTMLElement {
     set wcname(value) {
         this.setAttribute("wcname", value);
     }
-
+    // 
     updateSlot(elem) {
         if (!elem instanceof HTMLElement || elem instanceof popperCover) {
             return;
         }
-        this.shadowRoot.querySelector("[name = 'docker']").appendChild(elem);
+        elem.setAttribute('slot', 'docker');
+        this.appendChild(elem);        
+        this.updateRef(this.refNode);
+        // let new_element = this.refNode;        
+        
+    };
+    //
+    updateRef(new_element){
+        new_element.attrPanel = new Popper(new_element, this.shadowRoot.querySelector('.popper'), {
+            placement: 'right',
+            modifiers: {
+                flip: {
+                    behavior: ['left', 'bottom', 'top']
+                },
+                preventOverflow: {
+                    // boundariesElement: container,
+                },
+                arrow: { enabled: true }
+            },
+            onCreate: function (data) {
+                console.log('onCreate:', data, this);
+                document.querySelectorAll('pop-cover').forEach(function (elem) {                                                
+                    if (elem) {
+                        // elem.shadowRoot.querySelector('.popper').style.display = 'none';
+                        elem.close();
+                    }
+                })
+
+            }
+        });
+        new_element.addEventListener('wc_click', function (el) {
+            new_element.attrPanel.update();
+            // close all other popper
+            document.querySelectorAll('pop-cover').forEach(function (elem) {
+                if (elem && elem.refNode !== new_element) {
+                    // elem.shadowRoot.querySelector('.popper').style.display = 'none';
+                    elem.close();
+                }
+            })
+            // toggle popper of (this) new_element
+            new_element.attrPanel.popper.style.display = new_element.attrPanel.popper.style.display ==
+                'none' ? 'block' : 'none'
+
+        }, false);
     }
+    // 
     render() {
         const shadowRoot = (this.shadowRoot == null) ?
             this.attachShadow({
@@ -56,16 +100,17 @@ class popperCover extends HTMLElement {
         const template = shadowRoot.querySelector("#pop-cover-template");
         const instance = template.content.cloneNode(true);
         shadowRoot.appendChild(instance);
-        // this.dispatchEvent(new CustomEvent('WebComponentsReady', {}));
+        this.dispatchEvent(new CustomEvent('loadComplete'));
         var externalObj = this.getAttribute('externalObj');
         
         
         shadowRoot.querySelector("[comp-role = 'close']").addEventListener('click', ()=>{
-            this.dispatchEvent(new CustomEvent('wc_close'));    
+            this.close()  ;
         });
     }
     close(){
-        this.dispatchEvent(new CustomEvent('wc_close'));
+        this.dispatchEvent(new CustomEvent('wc_close'));        
+        this.shadowRoot.querySelector('.popper').style.display = "none";
     }
 
 }
