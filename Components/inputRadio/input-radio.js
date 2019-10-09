d7efc1f4a049da67ea748b6e214e2eadae26175e
PhanHoangAnh <hoangnanh17b@gmail.com>
2019-10-09 11:21:38 +0700

    Start working with inputRadio

--- Components/inputRadio/input-radio.js
+++ Components/inputRadio/input-radio.js
@@ -1,11 +1,85 @@
 
-const _html = ` <template id="input-radio-template">
-<link rel="stylesheet" href="inputRadio/inputRadio.css">
-  <h3 class="">
-    <span class=""></span>
-        <input-radio-template>I am a inputRadio.js </input-radio-template> - webcomponent
-    <span class=""></span>
-  </h3>
+const _html = ` 
+<template id='main'>
+<div class="row">
+    <div class="col-md-3 col-lg-3"><label att-prop="label">Radio Input</label></div>
+    <div class="col-md-9 col-lg-9" component-role="inputCover">
+        <div class="col-md-12 col-lg-12 " component-role="optionHolder">
+            <!--  -->
+            <div class="custom-control custom-radio">
+                <input type="radio" id="contactChoice1" name="contact" value="email"
+                    class="custom-control-input">
+                <label for="contactChoice1" class="custom-control-label">Email</label>
+            </div>
+            <!--  -->
+            <div class="custom-control custom-radio">
+                <input type="radio" id="contactChoice2" name="contact" value="phone"
+                    class="custom-control-input">
+                <label for="contactChoice2" class="custom-control-label">Phone</label>
+            </div>
+            <!--  -->
+            <div class="custom-control custom-radio">
+                <input type="radio" id="contactChoice3" name="contact" value="mail"
+                    class="custom-control-input">
+                <label for="contactChoice3" class="custom-control-label">Mail</label>
+            </div>
+            <!--  -->
+
+        </div>
+        <span class="help-block col-md-12 col-lg-12">Attribute Displaying Name</span>
+    </div>
+</div>
+</template>
+
+<template id='attPanel'>
+<div class="row">
+    <div class="container card" style="padding: 15px;">
+        <h4 class="card-title"><a att-title="">Attribute</a>
+            <button type="button" class="close" aria-label="Close" comp-role="close">
+                <span aria-hidden="true">×</span>
+            </button>
+        </h4>
+        <div class="row" style="margin-top: 10px;">
+            <div class="col-md-3 col-lg-3"><label>ID/Name</label></div>
+            <div class="col-md-9 col-lg-9">
+                <input class="col-md-12 col-lg-12 form-control" type="text" placeholder="placeholder"
+                    att-prop="name">
+                <span class="help-block col-md-12 col-lg-12">Name's attribute
+                </span>
+            </div>
+        </div>
+
+        <input-tag nodup placeholder="placeholder" label="Options" , placeholder='options' ,
+            description="control options"></input-tag>
+
+
+        <div class="row" style="margin-top: 10px;">
+            <div class="col-md-3 col-lg-3"><label>Label</label></div>
+            <div class="col-md-9 col-lg-9">
+                <input class="col-md-12 col-lg-12 form-control" type="text" placeholder="placeholder"
+                    att-prop="label">
+                <span class="help-block col-md-12 col-lg-12">Attribute Displaying Name
+                </span>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 10px;">
+            <div class="col-md-3 col-lg-3"><label>Description</label></div>
+            <div class="col-md-9 col-lg-9">
+                <input class="col-md-12 col-lg-12 form-control" type="text" placeholder="description"
+                    att-prop="description">
+                <span class="help-block col-md-12 col-lg-12">Attribute Description
+                </span>
+            </div>
+        </div>
+        <div class="container">
+            <div class="col text-center">
+                <button type="button" class="btn btn-primary" id="bntSample" comp-role="close">Save and
+                    Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 `
 export default _html;
--- Components/inputRadio/inputRadio.js
+++ Components/inputRadio/inputRadio.js
@@ -1,24 +1,19 @@
 
 //var currentDocument = document.currentScript.ownerDocument;
 import _html from "./input-radio.js";
+import uuidv4 from "../../Libs/uuid.js"
+import _css from "./inputRadio.css.js";
 
 class inputRadio extends HTMLElement {
     constructor() {
         super();
         this.addEventListener("click", (event) => {
-           // Do something here
+            // Do something here
         }, false)
     }
     connectedCallback() {
-        const shadowRoot = this.attachShadow({
-            mode: 'open'
-        });
-        shadowRoot.innerHTML = _html;
-        const template = shadowRoot.querySelector("#input-radio-template");
-        const instance = template.content.cloneNode(true);
-        shadowRoot.appendChild(instance);
-
-        var externalObj = this.getAttribute('externalObj');
+        this.render();
+        this.mountingAttPanel();
     }
 
     static get observedAttributes() {
@@ -26,7 +21,7 @@ class inputRadio extends HTMLElement {
     }
 
     attributeChangedCallback(name, oldVal, newVal) {
-        
+
         console.log('attributeChangeCalback:', name, oldVal, newVal);
         // if (name === "visible" && this.shadowRoot) {
         //     if (newVal === null) {
@@ -35,7 +30,106 @@ class inputRadio extends HTMLElement {
         //         this.shadowRoot.querySelector('[wrapper]').classList.add("visible");
         //     }
     }
+    render() {
+        // Listener
+        // Mounting element from template        
+        if (this.shadowRoot) {
+            return;
+        }
+        const shadowRoot = this.attachShadow({
+            mode: 'open'
+        });
 
+        // let id = UUID .generate(); console.log("uuid", id);
+        this.removeAttribute('att_uuid');
+        if (this.getAttribute('mode') !== 'config'){
+            this.setAttribute('att_uuid', uuidv4());    
+        }        
+
+        shadowRoot.innerHTML = _html;
+        let css = document.createElement('style')
+        css.innerHTML = _css;
+        shadowRoot.insertBefore(css, shadowRoot.firstElementChild);
+        // There are three modes "Input" or null mode as default -which shoud be used for input data, "config" mode used for setting attribues such as -label- -placeholder- -name- ...etc
+        const template = (this.getAttribute('mode') == 'config') ? shadowRoot.querySelector("#attPanel") : shadowRoot.querySelector("#main");
+        const instance = template.content.cloneNode(true);
+        shadowRoot.appendChild(instance);
+
+        if (this.getAttribute('mode') == 'config') {
+            let bnts = shadowRoot.querySelectorAll('[comp-role = "close"]').forEach(bnt => {
+                bnt.addEventListener('click', () => {
+                    this.dispatchEvent(new CustomEvent('_close', {
+                        detail: {
+                            'name': 'chipl'
+                        }
+                    }));
+                })
+            })
+        };
+        this.addEventListener('click', () => {
+            if (this.hasAttribute('noclick')) {
+                return;
+            }
+            this.dispatchEvent(new CustomEvent('_click', {
+                detail: {
+                    elem: this
+                },
+                bubbles: false
+            }));
+            if (this.attPanel) {
+                this.attPanel.refElem = this;
+                this.attPanel.updateAttPanel(this.C_DATA);
+            }
+
+        });        
+
+    }
+    // Acting as Attribute Panel for other intances
+    mountingAttPanel() {
+        this.shadowRoot.querySelectorAll('[att-prop]').forEach(el => {
+
+            el.addEventListener('change', () => {
+                let data = {};
+                console.log('Im here', el.value);
+                data[el.getAttribute('att-prop')] = el.value;
+                // 2. Second way, update directly to referenceElem
+                if (this.refElem) {
+                    this.refElem.updateInstance(data);
+                }
+                if (el.getAttribute('att-prop') == 'name') {
+                    this.shadowRoot.querySelector('[att-title]').innerHTML = data['name'];
+                }
+            })
+        })
+    };
+    //
+    // update Information for each element in webcomponent
+    updateInstance(data) {
+        if (data) {
+            Object.keys(data).forEach((k) => {
+                let elem = this.shadowRoot.querySelector(`[att-prop="${k}"]`);
+                if (elem && elem.innerHTML) {
+                    elem.innerHTML = data[k];
+                }
+                if (elem && elem.placeholder && k == "placeholder") {
+                    elem.placeholder = data[k];
+                }
+            });
+            let C_DATA = (this.C_DATA) ? this.C_DATA : {};
+            this.C_DATA = Object.assign({}, C_DATA, data);
+            this.setAttribute('c-data', true);
+        }
+    };
+    updateAttPanel(data) {
+        if (data) {
+            Object.keys(data).forEach((k) => {
+                this.shadowRoot.querySelector(`[att-prop="${k}"]`).value = data[k];                
+                if (k == 'name') {
+                    this.shadowRoot.querySelector('[att-title]').innerHTML = data['name'];
+                }
+            })
+        }
+    };
 }
 
 customElements.define("input-radio", inputRadio);
